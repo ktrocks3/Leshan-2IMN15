5ea936a8c5ef7a910ae95a3e4dabafb9f6f160ee
Kishan Thakurani <kishanthakurani2003@gmail.com>
2025-12-09 10:14:13 +0100

    PresenceDetector registration handled

--- leshan-server-demo/src/main/java/org/course/RoomControl.java
+++ leshan-server-demo/src/main/java/org/course/RoomControl.java
@@ -12,6 +12,7 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.io.PrintWriter;
 
+import org.eclipse.leshan.core.response.ResponseCallback;
 import org.eclipse.leshan.server.californium.LeshanServer;
 import org.eclipse.leshan.server.registration.Registration;
 import org.eclipse.leshan.core.response.WriteResponse;
@@ -27,7 +28,7 @@ import org.eclipse.leshan.core.observation.SingleObservation;
 
 
 public class RoomControl {
-    
+
     //
     // Static reference to the server.
     //
@@ -38,15 +39,14 @@ public class RoomControl {
     //
     // Declare variables to keep track of the state of the room.
     //
-    
-    public static void Initialize(LeshanServer server)
-    {
-	// Register the LWM2M server object for future use
-	lwServer = server;
 
-	// 2IMN15:  TODO  : fill in
-	//
-	// Initialize the state variables.
+    public static void Initialize(LeshanServer server) {
+        // Register the LWM2M server object for future use
+        lwServer = server;
+
+        // 2IMN15:  TODO  : fill in
+        //
+        // Initialize the state variables.
 
     }
 
@@ -57,73 +57,89 @@ public class RoomControl {
     // * set the power flag of all luminaires.
     // * show the status of the room.
 
-    
-    public static void handleRegistration(Registration registration)
-    {
+
+    public static void handleRegistration(Registration registration) {
         // Check which objects are available.
-        Map<Integer,org.eclipse.leshan.core.LwM2m.Version> supportedObject =
-	    registration.getSupportedObject();
+        Map<Integer, org.eclipse.leshan.core.LwM2m.Version> supportedObject =
+                registration.getSupportedObject();
 
         if (supportedObject.get(Constants.PRESENCE_DETECTOR_ID) != null) {
-	    System.out.println("Presence Detector");
+            System.out.println("Presence Detector:" + registration.getEndpoint());
+            // 2IMN15:  Process the registration of a new Presence Detector.
+            // 2IMN15:  The sequence diagram shows
+            // 			Presence detector -> Room control (Register ep=IoT-Pi42 Presence)
+            // 			Presence detector <- Room control (Created)
+            // 			Presence detector <- Room control (GET presence observe)
+            // 			Presence detector -> Room control (2.05 Content observe)
 
-	    //
-	    // 2IMN15:  TODO  :  fill in
-	    //
-	    // Process the registration of a new Presence Detector.
+            // The first one already happens; so we have to do the GET presence observe;
+            // So we subscribe to presence changes by observing the Presence resource
+            ObserveRequest requestPresence = new ObserveRequest(Constants.PRESENCE_DETECTOR_ID, 0, Constants.RES_PRESENCE);
+
+            try {
+                // 2IMN15: We can now send the request we made, and wait 5 seconds to get a response
+                ObserveResponse responsePresence = lwServer.send(registration, requestPresence, 5000);
+                // 2IMN15: If we don't get a response within 5 seconds, or we get a non successful response; print about the failure
+                if (responsePresence == null || !responsePresence.isSuccess()) {
+                    System.out.println("Failed to start observing presence for " + registration.getEndpoint());
+                } else {
+                    // 2IMN15: Else print about the success
+                    System.out.println("Started observing presence for " + registration.getEndpoint());
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         if (supportedObject.get(Constants.LUMINAIRE_ID) != null) {
-	    System.out.println("Luminaire");
+            System.out.println("Luminaire");
 
-	    //
-	    // 2IMN15:  TODO  :  fill in
-	    //
-	    // Process the registration of a new Luminaire.
+            //
+            // 2IMN15:  TODO  :  fill in
+            //
+            // Process the registration of a new Luminaire.
         }
 
         if (supportedObject.get(Constants.DEMAND_RESPONSE_ID) != null) {
-	    System.out.println("Demand Response");
-	    //
-	    // The registerDemandResponse() method contains example code
-	    // on how handle a registration. 
-	    //
-	    int powerBudget = registerDemandResponse(registration);
+            System.out.println("Demand Response");
+            //
+            // The registerDemandResponse() method contains example code
+            // on how handle a registration.
+            //
+            int powerBudget = registerDemandResponse(registration);
         }
 
-	//  2IMN15: don't forget to update the other luminaires.
+        //  2IMN15: don't forget to update the other luminaires.
     }
-    
 
-    public static void handleDeregistration(Registration registration)
-    {
-	//
-	// 2IMN15:  TODO  :  fill in
-	//
-	// The device identified by the given registration will
-	// disappear.  Update the state accordingly.
+
+    public static void handleDeregistration(Registration registration) {
+        //
+        // 2IMN15:  TODO  :  fill in
+        //
+        // The device identified by the given registration will
+        // disappear.  Update the state accordingly.
     }
-    
+
     public static void handleObserveResponse(SingleObservation observation,
-					     Registration registration,
-					     ObserveResponse response)
-    {
+                                             Registration registration,
+                                             ObserveResponse response) {
         if (registration != null && observation != null && response != null) {
-	    //
-	    // 2IMN15:  TODO  :  fill in
-	    //
-	    // When the registration and observation are known,
-	    // process the value contained in the response.
-	    //
-	    // Useful methods:
-	    //    registration.getEndpoint()
-	    //    observation.getPath()
-	    
+            //
+            // 2IMN15:  TODO  :  fill in
+            //
+            // When the registration and observation are known,
+            // process the value contained in the response.
+            //
+            // Useful methods:
+            //    registration.getEndpoint()
+            //    observation.getPath()
 
-	    // For processing an update of the Demand Response object.
-	    // It contains some example code.
-	    int newPowerBudget = observedDemandResponse(observation, response);
-	    
+
+            // For processing an update of the Demand Response object.
+            // It contains some example code.
+            int newPowerBudget = observedDemandResponse(observation, response);
+
         }
     }
 
@@ -132,152 +148,137 @@ public class RoomControl {
     // certain types.
 
     // Returns the current power budget.
-    private static int registerDemandResponse(Registration registration)
-    {
-	int powerBudget = readInteger(registration,
-				      Constants.DEMAND_RESPONSE_ID,
-				      0,
-				      Constants.RES_TOTAL_BUDGET);
-	System.out.println("Power budget is " + powerBudget);
-	// Observe the total budget information for updates.
-	try {
-	    ObserveRequest obRequest =
-		new ObserveRequest(Constants.DEMAND_RESPONSE_ID,
-				   0,
-				   Constants.RES_TOTAL_BUDGET);
-	    System.out.println(">>ObserveRequest created << ");
-	    ObserveResponse coResponse =
-		lwServer.send(registration, obRequest, 1000);
-	    System.out.println(">>ObserveRequest sent << ");
-	    if (coResponse == null) {
-		System.out.println(">>ObserveRequest null << ");
-	    }
-	}
-	catch (Exception e) {
-	    System.out.println("Observe request failed for Demand Response.");
-	}
-	return powerBudget;
+    private static int registerDemandResponse(Registration registration) {
+        int powerBudget = readInteger(registration,
+                Constants.DEMAND_RESPONSE_ID,
+                0,
+                Constants.RES_TOTAL_BUDGET);
+        System.out.println("Power budget is " + powerBudget);
+        // Observe the total budget information for updates.
+        try {
+            ObserveRequest obRequest =
+                    new ObserveRequest(Constants.DEMAND_RESPONSE_ID,
+                            0,
+                            Constants.RES_TOTAL_BUDGET);
+            System.out.println(">>ObserveRequest created << ");
+            ObserveResponse coResponse =
+                    lwServer.send(registration, obRequest, 1000);
+            System.out.println(">>ObserveRequest sent << ");
+            if (coResponse == null) {
+                System.out.println(">>ObserveRequest null << ");
+            }
+        } catch (Exception e) {
+            System.out.println("Observe request failed for Demand Response.");
+        }
+        return powerBudget;
     }
 
     // If the response contains a new power budget, it returns that value.
     // Otherwise, it returns -1.
     private static int observedDemandResponse(SingleObservation observation,
-					      ObserveResponse response)
-    {
-	// Alternative code:
-	// String obsRes = observation.getPath().toString();
-	// if (obsRes.equals("/33002/0/30005")) 
-	LwM2mPath obsPath = observation.getPath();
-	if ((obsPath.getObjectId() == Constants.DEMAND_RESPONSE_ID) &&
-	    (obsPath.getResourceId() == Constants.RES_TOTAL_BUDGET)) {
-	    String strValue = ((LwM2mResource)response.getContent()).getValue().toString();
-	    try {
-		int newPowerBudget = Integer.parseInt(strValue);
+                                              ObserveResponse response) {
+        // Alternative code:
+        // String obsRes = observation.getPath().toString();
+        // if (obsRes.equals("/33002/0/30005"))
+        LwM2mPath obsPath = observation.getPath();
+        if ((obsPath.getObjectId() == Constants.DEMAND_RESPONSE_ID) &&
+                (obsPath.getResourceId() == Constants.RES_TOTAL_BUDGET)) {
+            String strValue = ((LwM2mResource) response.getContent()).getValue().toString();
+            try {
+                int newPowerBudget = Integer.parseInt(strValue);
 
-		return newPowerBudget;
-	    }
-	    catch (Exception e) {
-		System.out.println("Exception in reading demand response:" + e.getMessage());
-	    }	       
-	}
-	return -1;
+                return newPowerBudget;
+            } catch (Exception e) {
+                System.out.println("Exception in reading demand response:" + e.getMessage());
+            }
+        }
+        return -1;
     }
-    
-    
-    private static int readInteger(Registration registration, int objectId, int instanceId, int resourceId)
-    {
+
+
+    private static int readInteger(Registration registration, int objectId, int instanceId, int resourceId) {
         try {
-	    ReadRequest request = new ReadRequest(objectId, instanceId, resourceId);
-	    ReadResponse cResponse = lwServer.send(registration, request, 5000);
-	    if (cResponse.isSuccess()) {
-		String sValue = ((LwM2mResource)cResponse.getContent()).getValue().toString();
-		try {
-		    int iValue = Integer.parseInt(((LwM2mResource)cResponse.getContent()).getValue().toString());
-		    return iValue;
-		}
-		catch (Exception e) {
-		}
-		float fValue = Float.parseFloat(((LwM2mResource)cResponse.getContent()).getValue().toString());
-		return (int)fValue;
-	    } else {
-		return 0;
-	    }
-        }
-        catch (Exception e) {
-	    System.out.println(e.getMessage());
-	    System.out.println("readInteger: exception");
-	    return 0;
+            ReadRequest request = new ReadRequest(objectId, instanceId, resourceId);
+            ReadResponse cResponse = lwServer.send(registration, request, 5000);
+            if (cResponse.isSuccess()) {
+                String sValue = ((LwM2mResource) cResponse.getContent()).getValue().toString();
+                try {
+                    int iValue = Integer.parseInt(((LwM2mResource) cResponse.getContent()).getValue().toString());
+                    return iValue;
+                } catch (Exception e) {
+                }
+                float fValue = Float.parseFloat(((LwM2mResource) cResponse.getContent()).getValue().toString());
+                return (int) fValue;
+            } else {
+                return 0;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("readInteger: exception");
+            return 0;
         }
     }
-    
-    private static String readString(Registration registration, int objectId, int instanceId, int resourceId)
-    {
+
+    private static String readString(Registration registration, int objectId, int instanceId, int resourceId) {
         try {
-	    ReadRequest request = new ReadRequest(objectId, instanceId, resourceId);
-	    ReadResponse cResponse = lwServer.send(registration, request, 1000);
-	    if (cResponse.isSuccess()) {
-		String value = ((LwM2mResource)cResponse.getContent()).getValue().toString();
-		return value;
-	    } else {
-		return "";
-	    }
+            ReadRequest request = new ReadRequest(objectId, instanceId, resourceId);
+            ReadResponse cResponse = lwServer.send(registration, request, 1000);
+            if (cResponse.isSuccess()) {
+                String value = ((LwM2mResource) cResponse.getContent()).getValue().toString();
+                return value;
+            } else {
+                return "";
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("readString: exception");
+            return "";
         }
-        catch (Exception e) {
-	    System.out.println(e.getMessage());
-	    System.out.println("readString: exception");
-	    return "";
+    }
+
+    private static void writeInteger(Registration registration, int objectId, int instanceId, int resourceId, int value) {
+        try {
+            WriteRequest request = new WriteRequest(objectId, instanceId, resourceId, value);
+            WriteResponse cResponse = lwServer.send(registration, request, 1000);
+            if (cResponse.isSuccess()) {
+                System.out.println("writeInteger: Success");
+            } else {
+                System.out.println("writeInteger: Failed, " + cResponse.toString());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("writeInteger: exception");
         }
     }
-    
-    private static void writeInteger(Registration registration, int objectId, int instanceId, int resourceId, int value)
-    {
-	try {
-	    WriteRequest request = new WriteRequest(objectId, instanceId, resourceId, value);
-	    WriteResponse cResponse = lwServer.send(registration, request, 1000);
-	    if (cResponse.isSuccess()) {
-		System.out.println("writeInteger: Success");
-	    } else {
-		System.out.println("writeInteger: Failed, " + cResponse.toString());
-	    }
-	}
-	catch (Exception e) {
-	    System.out.println(e.getMessage());
-	    System.out.println("writeInteger: exception");
-	}
+
+    private static void writeString(Registration registration, int objectId, int instanceId, int resourceId, String value) {
+        try {
+            WriteRequest request = new WriteRequest(objectId, instanceId, resourceId, value);
+            WriteResponse cResponse = lwServer.send(registration, request, 1000);
+            if (cResponse.isSuccess()) {
+                System.out.println("writeString: Success");
+            } else {
+                System.out.println("writeString: Failed, " + cResponse.toString());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("writeString: exception");
+        }
     }
-    
-    private static void writeString(Registration registration, int objectId, int instanceId, int resourceId, String value)
-    {
-	try {
-	    WriteRequest request = new WriteRequest(objectId, instanceId, resourceId, value);
-	    WriteResponse cResponse = lwServer.send(registration, request, 1000);
-	    if (cResponse.isSuccess()) {
-		System.out.println("writeString: Success");
-	    } else {
-		System.out.println("writeString: Failed, " + cResponse.toString());
-	    }
-	}
-	catch (Exception e) {
-	    System.out.println(e.getMessage());
-	    System.out.println("writeString: exception");
-	}
+
+    private static void writeBoolean(Registration registration, int objectId, int instanceId, int resourceId, boolean value) {
+        try {
+            WriteRequest request = new WriteRequest(objectId, instanceId, resourceId, value);
+            WriteResponse cResponse = lwServer.send(registration, request, 1000);
+            if (cResponse.isSuccess()) {
+                System.out.println("writeBoolean: Success");
+            } else {
+                System.out.println("writeBoolean: Failed, " + cResponse.toString());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("writeBoolean: exception");
+        }
     }
-    
-    private static void writeBoolean(Registration registration, int objectId, int instanceId, int resourceId, boolean value)
-    {
-	try {
-	    WriteRequest request = new WriteRequest(objectId, instanceId, resourceId, value);
-	    WriteResponse cResponse = lwServer.send(registration, request, 1000);
-	    if (cResponse.isSuccess()) {
-		System.out.println("writeBoolean: Success");
-	    } else {
-		System.out.println("writeBoolean: Failed, " + cResponse.toString());
-	    }
-	}
-	catch (Exception e) {
-	    System.out.println(e.getMessage());
-	    System.out.println("writeBoolean: exception");
-	}
-    }
-    
+
 }
